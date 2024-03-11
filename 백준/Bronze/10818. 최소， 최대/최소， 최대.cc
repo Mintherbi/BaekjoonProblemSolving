074ea856e4dde726a6e51231faf55e5637a09b10
MinsupChung <109859956+Mintherbi@users.noreply.github.com>
2024-03-11 22:21:50 +0900

    [Bronze III] Title: 최소, 최대, Time: 456 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    int arr[1000000];
+
+    cin >> N;
+
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+
+    int Max = arr[0], Min = arr[0];
+
+    for(int j=0; j<N; j++){
+        if(Max < arr[j]){
+            Max = arr[j];
+        }
+        if(Min > arr[j]){
+            Min = arr[j];
+        }
+    }
+
+    cout << Min << " " << Max;
+
+    return 0;
+}
