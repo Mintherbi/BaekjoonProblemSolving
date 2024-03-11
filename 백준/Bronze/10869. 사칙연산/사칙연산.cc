7969b1db47f92c9c056a96018345bb8275af05dd
MinsupChung <109859956+Mintherbi@users.noreply.github.com>
2024-03-11 22:21:59 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    int A; 
+    int B;
+
+    std::cin >> A;
+    std::cin >> B;
+
+    std::cout << A + B << std::endl;
+    std::cout << A - B << std::endl;
+    std::cout << A * B << std::endl;
+    std::cout << A / B << std::endl;
+    std::cout << A % B << std::endl;
+
+
+    return 0;
+}
