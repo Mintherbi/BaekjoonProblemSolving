596bd9ae51e1fa8d032f6fa0b2acab8019989760
MinsupChung <109859956+Mintherbi@users.noreply.github.com>
2024-03-11 22:22:21 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int N=0;
+    int Result=1;
+    cin>>N;
+
+    for(int x=1; x<=N; x++){
+        Result*=x;
+    }
+    cout<<Result;
+
+    return 0;
+}
