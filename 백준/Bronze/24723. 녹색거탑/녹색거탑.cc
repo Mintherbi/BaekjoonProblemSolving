e93e3fa87bd4b685ca8c3131740d308e1e5e7409
MinsupChung <109859956+Mintherbi@users.noreply.github.com>
2024-03-11 22:25:11 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int N;
+    cin>>N;
+    cout<<pow(2,N);
+    return 0;
+}
