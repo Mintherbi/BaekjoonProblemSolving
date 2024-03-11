e554ae6fcc45ac769236d1796585c65faa467391
MinsupChung <109859956+Mintherbi@users.noreply.github.com>
2024-03-11 22:24:22 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int N;
+
+    cin>>N;
+    cout<<N*(N-1);
+
+    return 0;
+}
