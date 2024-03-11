9af72e8508f4a970264e9bb46e96feb6e1d61b23
MinsupChung <109859956+Mintherbi@users.noreply.github.com>
2024-03-11 22:21:29 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int M,N;
+
+    cin>>N>>M;
+
+    vector<int> Basket(N);
+
+    for(int z=0; z<N; z++){
+        Basket[z]=z+1;
+    }
+
+    for(int x=0; x<M; x++){
+        int i,j;
+        cin>>i>>j;
+
+        for(int a=i, b=j; a<b; a++,b--){
+            int temp=Basket[a-1];
+            Basket[a-1]=Basket[b-1];
+            Basket[b-1]=temp;
+        }
+    }
+
+    for(int y; y<N;y++){
+        cout<<Basket[y]<<" ";
+    }
+
+    return 0;
+}
