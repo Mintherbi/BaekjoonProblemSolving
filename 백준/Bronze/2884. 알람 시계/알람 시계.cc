8fe76e741ab68a475d8111301285c16a894378bb
MinsupChung <109859956+Mintherbi@users.noreply.github.com>
2024-03-11 22:19:38 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        int t,m;
+
+        cin >> t >> m;
+
+        if (m >= 45 && m < 60)
+        {
+                cout << t << " " << m-45 << endl;
+        }
+        else if (m<45)
+        {
+                if(t==0) cout << 23 << " " << m+15;
+                else cout << t-1 << " " << m+15 << endl;
+        }
+
+        return 0;
+}
